7bb1aba95d6100ad1aad3bc9d48b0c00ebfa2fd3
SmallDumplings <katyachecryglina@gmail.com>
2021-05-16 23:30:38 +0500

    hz2com

--- app/src/main/java/com/example/project/one_oneFragment.java
+++ app/src/main/java/com/example/project/one_oneFragment.java
@@ -15,6 +15,7 @@ import android.widget.ImageView;
 
 public class one_oneFragment extends Fragment {
     ImageButton ib1, ib2, ib3, ib4, ib5, ib6, ib7,ib8, ib9, ib10, ib11, ib12, ib13, ib14,ib15,ib16,ib17,ib18,ib19,ib20,ib21,ib22,ib23,ib24,ib25,ib26,ib27,ib28;
+    String s;
 
    //фрагмент с картинками 1 типа
 
@@ -23,16 +24,19 @@ public class one_oneFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_one_one, container, false);
+        ib1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forButton(s ="R.logo1.");
+            }
+        });
 
     }
-    public void forButton(ImageView image){
 
 
-
+    public void forButton(String image){
         Intent intent = new Intent(one_oneFragment.this, TwoClass.class);
         intent.putExtra("image", image);
         startActivity(intent);
-
-
     }
 }
